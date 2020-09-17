ec53801a411c253cf061b61d3773cbce18fec713
weiss <weiss.personal@gmail.com>
2020-09-17 09:03:19 -0700

    image data passed in FitsData message passed to FitsActor

--- simulated-infrared-detector/src/main/scala/org/tmt/osw/simulatedinfrareddetector/ControllerData.scala
+++ simulated-infrared-detector/src/main/scala/org/tmt/osw/simulatedinfrareddetector/ControllerData.scala
@@ -9,6 +9,9 @@ case object Exposing extends ControllerState
 case object Aborting extends ControllerState
 
 case class ExposureParameters(integrationTimeMillis: Int, coadds: Int)
+case class FitsData(data: Array[Array[Int]]) {
+  val dimensions: (Int, Int) = (data.length, data(0).length)
+}
 
 case class ControllerData(logger: Logger,
                           state: ControllerState,
